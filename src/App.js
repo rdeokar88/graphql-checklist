7de904c0eb0535114e79bd1546398c1f09d9819c
Ankita Pawar <ankitapawar@Ankitas-MacBook-Pro.local>
2020-08-29 19:43:15 -0700

    React with Apollo graphql

--- src/App.js
+++ src/App.js
@@ -1,24 +1,111 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useQuery, gql, useMutation } from '@apollo/client';
+
+const GET_TODOS = gql `
+  query getTodos {
+    todos {
+      done
+      id
+      text
+    }
+  }
+`;
+
+const TOGGLE_TODOS = gql `
+  mutation toggleTodos($id: uuid!, $done: Boolean!) {
+    update_todos(where: {id: {_eq: $id}}, _set: {done: $done}) {
+      returning {
+        done
+        id
+        text
+      }
+    }
+  }
+`;
+
+const ADD_TODO = gql `
+  mutation addTodos($text: String!) {
+    insert_todos(objects: {text: $text}) {
+      returning {
+        done
+        id
+        text
+      }
+    }
+  }
+`;
+
+const DELETE_TODO = gql `
+  mutation MyMutation($id: uuid!) {
+    delete_todos(where: {id: {_eq: $id}}) {
+      returning {
+        id
+      }
+    }
+  }
+`;
 
 function App() {
+  const [ todos, setTodos ] = React.useState('');
+  const { data, loading, error } = useQuery(GET_TODOS);
+  const [ toggleTodo ] = useMutation(TOGGLE_TODOS);
+  const [ addTodo ] = useMutation(ADD_TODO, {
+    onCompleted: () => setTodos('')
+  });
+  const [ deletTodos ] = useMutation(DELETE_TODO);
+
+
+  if(error) return <div>Error Fetching todos. Try again later</div>
+  if (loading) return <div>Loading...</div>
+
+  const handleTodosChange = (event) => {
+    setTodos(event.target.value);
+  };
+
+  const handleTodosSubmit = async (event) => {
+    event.preventDefault();
+    if(!todos.trim()) return;
+    await addTodo({ variables: { text: todos }, 
+      refetchQueries: [
+        {
+          query: GET_TODOS
+        }
+      ]
+    });
+  };
+
+  const handleToggleTodos = async ({ id, done }) => {
+    await toggleTodo({ variables: { id: id, done: !done }} );
+  };
+
+  const handleDeleteTodo = async ({ id }) => {
+    await deletTodos({ variables: { id: id } ,
+      update : (cache) => {
+        const prevData = cache.readQuery({ query: GET_TODOS});
+        const newData = prevData.todos.filter(todo => todo.id !== id);
+        cache.writeQuery({ query: GET_TODOS, data : { todos: newData }});
+      }
+    });
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="vh-100 code flex flex-column items-center bg-purple white">
+      <h1>GraphQL sample Todos app</h1>
+      <form className="mb3" onSubmit={handleTodosSubmit}>
+        <input className="pa2 f4" type="text" onChange={handleTodosChange} placeholder="Enter your todo" 
+          value={todos}/>
+        <button className="ml2 f4 bg-green" type="submit">Create Todo</button>
+      </form>
+      <div className="flex-items-center justify-center flex-column">
+        { data.todos.map(todo => (
+          <p onDoubleClick={() => handleToggleTodos(todo)} key={todo.id}>
+            <span className={ `pointer ${todo.done && 'strike'}` }>{todo.text}</span>
+            <button onClick={() => handleDeleteTodo(todo)} className="ml2 bg-transparent bn">
+              <span className="red">&times;</span>
+            </button>
+          </p>
+        ))}
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+const apololloClient = new ApolloClient( {
+  uri: 'https://selected-lacewing-63.hasura.app/v1/graphql',
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={apololloClient}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
